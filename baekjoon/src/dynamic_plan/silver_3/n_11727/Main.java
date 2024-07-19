7e75add0660e801e53c59e93a50c273ec8aeebea
GustavEiffels <sibuk9315@gmail.com>
2024-07-19 23:23:15 +0900

    타잃

--- baekjoon/src/dynamic_plan/silver_3/n_11727/Main.java
+++ baekjoon/src/dynamic_plan/silver_3/n_11727/Main.java
@@ -4,15 +4,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// ** https://www.acmicpc.net/problem/11727 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int   n  = Integer.parseInt(br.readLine());
-        int[] ho = new int[n+1];
-        int[] ve = new int[n+1];
+        int[] dp = new int[n+1];
 
-        ho[1] = 0;
-        ve[1] = 1;
+        if(n >= 1) dp[1] =  1;
+        if(n >= 2) dp[2] =  3;
 
+        for(int i = 3; i<=n; i++){
+            dp[i] = (dp[i-1]+2*dp[i-2])%10007;
+        }
+
+        System.out.println(dp[n]%10007);
     }
 }
